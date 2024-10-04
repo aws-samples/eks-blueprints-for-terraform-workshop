cb484935e9dd2d111cd07d7abd71c53539f16802
Workshop User <workshopuser@example.com>
2024-10-04 20:36:27 +0000

    fix during review
    Signed-off-by: Workshop User <workshopuser@example.com>

--- assets/scripts/destroy-applications.sh
+++ assets/scripts/destroy-applications.sh
@@ -4,19 +4,19 @@ set -uo pipefail
 
 
 #Activate AutoSync
-#kubectl  --context hub patch applicationset appofapps -n argocd --type=json -p='[{"op": "add", "path": "/spec/template/spec/syncPolicy", "value": {"automated": {"prune": true, "selfHeal": true}}}]'
+#kubectl  --context hub-cluster patch applicationset bootstrap -n argocd --type=json -p='[{"op": "add", "path": "/spec/template/spec/syncPolicy", "value": {"automated": {"prune": true, "selfHeal": true}}}]'
 #Deactivate auto-sync
-kubectl  --context hub patch applicationset appofapps -n argocd --type=json -p='[{"op": "remove", "path": "/spec/template/spec/syncPolicy"}]'
+kubectl  --context hub-cluster patch applicationset bootstrap -n argocd --type=json -p='[{"op": "remove", "path": "/spec/template/spec/syncPolicy"}]'
 
 #Clean Workloads
-kubectl  --context hub delete applicationset -n argocd workload --cascade=foreground
+kubectl  --context hub-cluster delete applicationset -n argocd workload --cascade=foreground
 #Clean namespaces
-kubectl  --context hub delete applicationset -n argocd namespace --cascade=foreground
+kubectl  --context hub-cluster delete applicationset -n argocd namespace --cascade=foreground
 #Clea projects
-kubectl  --context hub delete applicationset -n argocd argoprojects --cascade=foreground
+kubectl  --context hub-cluster delete applicationset -n argocd argoprojects --cascade=foreground
 #Clean addons (but the have preserve On)
-kubectl  --context hub delete applicationset -n argocd cluster-addons --cascade=foreground
+kubectl  --context hub-cluster delete applicationset -n argocd cluster-addons --cascade=foreground
 
 #Clean app of apps
-kubectl  --context hub delete applicationset -n argocd appofapps --cascade=foreground
+kubectl  --context hub-cluster delete applicationset -n argocd appofapps --cascade=foreground
 
--- setup-git.sh
+++ setup-git.sh
@@ -11,14 +11,13 @@ ROOTDIR=$SCRIPTDIR
 GITOPS_DIR=${GITOPS_DIR:-$SCRIPTDIR/environment/gitops-repos}
 echo $GITOPS_DIR
 
-PROJECT_CONTECXT_PREFIX=${PROJECT_CONTECXT_PREFIX:-eks-fleet-workshop-gitops}
+PROJECT_CONTECXT_PREFIX=${PROJECT_CONTECXT_PREFIX:-eks-blueprint-workshop-gitops}
 # Clone and initialize the gitops repositories
 gitops_workload_url="$(aws secretsmanager get-secret-value --secret-id ${PROJECT_CONTECXT_PREFIX}-workloads --query SecretString --output text | jq -r .url)"
 GIT_USER="$(aws secretsmanager get-secret-value --secret-id ${PROJECT_CONTECXT_PREFIX}-workloads --query SecretString --output text | jq -r .username)"
 GIT_PASS="$(aws secretsmanager get-secret-value --secret-id ${PROJECT_CONTECXT_PREFIX}-workloads --query SecretString --output text | jq -r .password)"
 gitops_platform_url="$(aws secretsmanager get-secret-value --secret-id ${PROJECT_CONTECXT_PREFIX}-platform --query SecretString --output text | jq -r .url)"
 gitops_addons_url="$(aws secretsmanager   get-secret-value --secret-id ${PROJECT_CONTECXT_PREFIX}-addons --query SecretString --output text | jq -r .url)"
-gitops_fleet_url="$(aws secretsmanager   get-secret-value  --secret-id ${PROJECT_CONTECXT_PREFIX}-fleet --query SecretString --output text | jq -r .url)"
 
 # if IDE_URL is set then setup
 if [[ -n "${IDE_URL:-}" ]]; then
@@ -32,10 +31,9 @@ EOT
     git config --global credential.helper 'store'
     git config --global init.defaultBranch main
 else
-gitops_workload_url=${gitops_workload_url/#https:\/\//https:\/\/"$GIT_USER":"$GIT_PASS"@}
-gitops_platform_url=${gitops_platform_url/#https:\/\//https:\/\/"$GIT_USER":"$GIT_PASS"@}
-gitops_addons_url=${gitops_addons_url/#https:\/\//https:\/\/"$GIT_USER":"$GIT_PASS"@}
-gitops_fleet_url=${gitops_fleet_url/#https:\/\//https:\/\/"$GIT_USER":"$GIT_PASS"@}
+    gitops_workload_url=${gitops_workload_url/#https:\/\//https:\/\/"$GIT_USER":"$GIT_PASS"@}
+    gitops_platform_url=${gitops_platform_url/#https:\/\//https:\/\/"$GIT_USER":"$GIT_PASS"@}
+    gitops_addons_url=${gitops_addons_url/#https:\/\//https:\/\/"$GIT_USER":"$GIT_PASS"@}
 fi
 
 # Reset directory
@@ -43,52 +41,35 @@ rm -rf ${GITOPS_DIR}
 mkdir -p ${GITOPS_DIR}
 
 #git clone ${gitops_workload_url} ${GITOPS_DIR}/apps
-git init ${GITOPS_DIR}/apps
-git -C ${GITOPS_DIR}/apps remote add origin ${gitops_workload_url}
-cp -r ${ROOTDIR}/gitops/apps/*  ${GITOPS_DIR}/apps
-mkdir -p ${GITOPS_DIR}/apps/backend
-touch ${GITOPS_DIR}/apps/backend/.keep
-mkdir -p ${GITOPS_DIR}/apps/frontend
-touch ${GITOPS_DIR}/apps/frontend/.keep
+git init ${GITOPS_DIR}/workload
+git -C ${GITOPS_DIR}/workload remote add origin ${gitops_workload_url}
+cp -r ${ROOTDIR}/assets/workload/*  ${GITOPS_DIR}/workload
 
-# Deploy the app for this workshop
-# TODO: review with the team
-cp -r ${ROOTDIR}/gitops/apps/* ${GITOPS_DIR}/apps/
-
-git -C ${GITOPS_DIR}/apps add . || true
-git -C ${GITOPS_DIR}/apps commit -m "initial commit" || true
-git -C ${GITOPS_DIR}/apps push -u origin main -f  || true
+git -C ${GITOPS_DIR}/workload add . || true
+git -C ${GITOPS_DIR}/workload commit -m "initial commit" || true
+git -C ${GITOPS_DIR}/workload push -u origin main -f  || true
 
 # populate platform repository
 #git clone ${gitops_platform_url} ${GITOPS_DIR}/platform
 git init ${GITOPS_DIR}/platform
 git -C ${GITOPS_DIR}/platform remote add origin ${gitops_platform_url}
 mkdir -p ${GITOPS_DIR}/platform/charts
-cp -r ${ROOTDIR}/gitops/platform/charts/*  ${GITOPS_DIR}/platform/charts/
+#cp -r ${ROOTDIR}/gitops/platform/charts/*  ${GITOPS_DIR}/platform/charts/
 mkdir -p ${GITOPS_DIR}/platform/bootstrap
-cp -r ${ROOTDIR}/gitops/platform/bootstrap/*  ${GITOPS_DIR}/platform/bootstrap/
+#cp -r ${ROOTDIR}/gitops/platform/bootstrap/*  ${GITOPS_DIR}/platform/bootstrap/
 
 # Deploy the namespaces for this workshop
 # TODO: review with the team
 mkdir -p ${GITOPS_DIR}/platform/teams
-cp -r ${ROOTDIR}/gitops/platform/teams/*  ${GITOPS_DIR}/platform/teams/
+cp -r ${ROOTDIR}/assets/platform/*  ${GITOPS_DIR}/platform/
 
 git -C ${GITOPS_DIR}/platform add . || true
 git -C ${GITOPS_DIR}/platform commit -m "initial commit" || true
 git -C ${GITOPS_DIR}/platform push -u origin main -f || true
 
-#git clone ${gitops_addons_url} ${GITOPS_DIR}/addons
 git init ${GITOPS_DIR}/addons
 git -C ${GITOPS_DIR}/addons remote add origin ${gitops_addons_url}
-cp -r ${ROOTDIR}/gitops/addons/* ${GITOPS_DIR}/addons/
+cp -r ${ROOTDIR}/assets/platform/addons/* ${GITOPS_DIR}/addons/
 git -C ${GITOPS_DIR}/addons add . || true
 git -C ${GITOPS_DIR}/addons commit -m "initial commit" || true
 git -C ${GITOPS_DIR}/addons push -u origin main -f  || true
-
-#git clone ${gitops_fleet_url} ${GITOPS_DIR}/fleet
-git init ${GITOPS_DIR}/fleet
-git -C ${GITOPS_DIR}/fleet remote add origin ${gitops_fleet_url}
-cp -r ${ROOTDIR}/gitops/fleet/* ${GITOPS_DIR}/fleet/
-git -C ${GITOPS_DIR}/fleet add . || true
-git -C ${GITOPS_DIR}/fleet commit -m "initial commit" || true
-git -C ${GITOPS_DIR}/fleet push -u origin main -f || true
