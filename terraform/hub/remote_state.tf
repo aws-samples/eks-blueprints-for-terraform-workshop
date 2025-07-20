14dbb0f5c6afc0ad709b831a5d8ec335028f61f5
Satish Patil <patilsb@amazon.com>
2025-07-20 10:10:08 -0500

    Update remote_state.tf files to use S3 backend and modify bootstrap.sh to create them

--- terraform/hub/remote_state.tf
+++ terraform/hub/remote_state.tf
@@ -1,7 +1,15 @@
 data "terraform_remote_state" "vpc" {
-  backend = "local"
+  backend = "s3"
 
   config = {
-    path = "${path.module}/../vpc/terraform.tfstate"
+    bucket = "${data.aws_ssm_parameter.tfstate_bucket.value}"
+    key    = "vpc/terraform.tfstate"
+    region = "${data.aws_region.current.name}"
   }
 }
+
+data "aws_ssm_parameter" "tfstate_bucket" {
+  name = "eks-blueprints-workshop-tf-backend-bucket"
+}
+
+data "aws_region" "current" {}
