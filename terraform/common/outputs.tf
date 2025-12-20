aab62f1e7fea301a69707d5dafe48a38f309d125
Satish Patil <satishpatil@hotmail.com>
2025-12-20 13:58:00 -0600

    redesign

--- terraform/common/outputs.tf
+++ terraform/common/outputs.tf
@@ -1,13 +1,13 @@
 
 # This output is informational only and is not used by any other module
-output "git_secrets_urls" {
-  value       = local.git_secrets_urls
-  description = "The URLs for the Git repositories"
-}
-output "git_secrets_names" {
-  value       = local.git_secrets_names
-  description = "The names of the AWS Secrets for the Git repositories"
-}
+# output "git_secrets_urls" {
+#   value       = local.git_secrets_urls
+#   description = "The URLs for the Git repositories"
+# }
+# output "git_secrets_names" {
+#   value       = local.git_secrets_names
+#   description = "The names of the AWS Secrets for the Git repositories"
+# }
 
 output "gitops_user_name" {
   value       = var.gitea_user
